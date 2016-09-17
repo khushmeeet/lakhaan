cb60cf1a59fe893196358a062aecac0d1c2809c4
Khushmeet <khushmeetsingh1996@gmail.com>
2016-09-17 19:43:48 +0530

    redefined restaurant schema

--- server/config/seed.js
+++ server/config/seed.js
@@ -10,66 +10,130 @@ import User from '../api/user/user.model';
 Restaurant.find({}).removeAsync()
   .then(() => {
     Restaurant.create({
-      name:'Saleem',
-      speciality:['Non-Veg','Veg'],
-      menu:[{
-        name:'Chicken Bhasha',
-        price:75
-      },{
-        name:'Paneer Bhasha',
-        price:65
-      },{
-        name:'Egg Roll',
-        price:50
-      }]
+      name: 'Saleem',
+      speciality: ['Non-Veg', 'Veg'],
+      menu: {
+          'Main Course Non Veg': [{
+            name: 'Chicken Bhasha',
+            price: 75
+          }, {
+            name: 'Paneer Bhasha',
+            price: 65
+          }, {
+            name: 'Egg Roll',
+            price: 50
+          }],
+          'Main Course Veg': [{
+            name: 'Chicken Bhasha',
+            price: 75
+          }, {
+            name: 'Paneer Bhasha',
+            price: 65
+          }, {
+            name: 'Egg Roll',
+            price: 50
+          }]
+      },
+      address: 'Sector 41A, Chandigarh'
     },{
-      name:'Apna Dhaba',
-      speciality:['Non-Veg','Veg'],
-      menu:[{
-        name:'Kadai Paneer',
-        price:55
+      name: 'Apna Dhaba',
+      speciality: ['Non-Veg', 'Veg'],
+      menu: {
+          'Main Course Non Veg': [{
+            name: 'Chicken Bhasha',
+            price: 75
+          }, {
+            name: 'Paneer Bhasha',
+            price: 65
+          }, {
+            name: 'Egg Roll',
+            price: 50
+          }],
+          'Main Course Veg': [{
+            name: 'Chicken Bhasha',
+            price: 75
+          }, {
+            name: 'Paneer Bhasha',
+            price: 65
+          }, {
+            name: 'Egg Roll',
+            price: 50
+          }]
+        },
+        address: 'Sector 41A, Chandigarh'
       },{
-        name:'Butter Chicken',
-        price:70
-      }]
-    },{
-      name:'Flo caffee',
-      speciality:['Non-Veg','Veg'],
-      menu:[{
-        name:'Cake',
-        price:110
+      name: 'Flo caffee',
+      speciality: ['Non-Veg', 'Veg'],
+      menu: {
+          'Main Course Non Veg': [{
+            name: 'Chicken Bhasha',
+            price: 75
+          }, {
+            name: 'Paneer Bhasha',
+            price: 65
+          }, {
+            name: 'Egg Roll',
+            price: 50
+          }],
+          'Main Course Veg': [{
+            name: 'Chicken Bhasha',
+            price: 75
+          }, {
+            name: 'Paneer Bhasha',
+            price: 65
+          }, {
+            name: 'Egg Roll',
+            price: 50
+          }]
+        },
+        address: 'Sector 41A, Chandigarh'
       },{
-        name:'Pastry',
-        price:80
-      }]
-    },{
-      name:'Flo caffee',
-      speciality:['Non-Veg','Veg'],
-      menu:[{
-        name:'Cake',
-        price:110
-      },{
-        name:'Pastry',
-        price:80
-      }]
+      name: 'Flo caffee',
+      speciality: ['Non-Veg', 'Veg'],
+      menu: {
+          'Main Course Non Veg': [{
+            name: 'Chicken Bhasha',
+            price: 75
+          }, {
+            name: 'Paneer Bhasha',
+            price: 65
+          }, {
+            name: 'Egg Roll',
+            price: 50
+          }],
+          'Main Course Veg': [{
+            name: 'Chicken Bhasha',
+            price: 75
+          }, {
+            name: 'Paneer Bhasha',
+            price: 65
+          }, {
+            name: 'Egg Roll',
+            price: 50
+          }]
+      },
+      address: 'Sector 41A, Chandigarh'
+    })
+    .then(() => {
+      console.log('finished populating restaurants');
     })
   });
 
 User.find({}).removeAsync()
   .then(() => {
     User.createAsync({
-      provider: 'local',
-      name: 'Test User',
-      email: 'test@example.com',
-      password: 'test'
-    }, {
-      provider: 'local',
-      role: 'admin',
-      name: 'Admin',
-      email: 'admin@example.com',
-      password: 'admin'
-    })
-    .then(() => {
-      console.log('finished populating users');
-    });
+        provider: 'local',
+        name: 'Test User',
+        email: 'test@example.com',
+        password: 'test'
+      }, {
+        provider: 'local',
+        role: 'admin',
+        name: 'Admin',
+        email: 'admin@example.com',
+        password: 'admin'
+      })
+      .then(() => {
+        console.log('finished populating users');
+      });
   });
