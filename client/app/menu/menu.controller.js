515b79f640e2d85cfed6806a1a0679c8d68eb4c1
Khushmeet <khushmeetsingh1996@gmail.com>
2016-10-17 01:03:47 +0530

    fixes

--- client/app/menu/menu.controller.js
+++ client/app/menu/menu.controller.js
@@ -1,46 +1,40 @@
 'use strict';
 
 angular.module('lakhaanaApp')
-  .controller('MenuCtrl',function($scope,$stateParams,$location,menuService,Auth,shareData,userService){
-    $scope.order = [];
-    var i=0;
+  .controller('MenuCtrl', function ($scope, $stateParams, $location, menuService, Auth, shareData, userService) {
+    $scope.order = {};
+    var i = 0;
     $scope.quantity = 1;
-    menuService.get({id:$stateParams.id},
-      function success(response){
-        $scope.res_obj = {name: response.name, spec: response.speciality, address: response.address};
+    menuService.get({ id: $stateParams.id },
+      function success(response) {
+        $scope.res_obj = { name: response.name, spec: response.speciality, address: response.address };
         $scope.menuObject = response.menu;
         $scope.keys = Object.keys($scope.menuObject);
       },
-      function error(errorResponse){
+      function error(errorResponse) {
         console.log(errorResponse);
       });
 
-      $scope.addItem = function(quantity,name,price){
-        var obj={'name':name,'quantity':quantity,'price':price};
-        $scope.order[i]=obj;
-        i++;
-        $scope.quantity = 1;
-      };
+    $scope.addItem = function (quantity, name, price) {
+      var obj = { 'name': name, 'quantity': quantity, 'price': price };
+      $scope.order[i] = obj;
+      i++;
+      $scope.quantity = 1;
+    };
 
-      $scope.deleteItem = function(item) {
-        var index = $scope.order.indexOf(item);
-        $scope.order.splice(index, 1);
-      }
+    $scope.deleteItem = function (item) {
+      var index = $scope.order.indexOf(item);
+      $scope.order.splice(index, 1);
+    }
 
-      $scope.checkout = function () {
-        var id = Auth.getCurrentUser()._id;
-        /*userService.update({id:id},{'order':[$scope.order]},
-          function success(response) {
-            console.log(response);
-          },
-          function error(error) {
-            console.log(error);
-          });*/
-          shareData.store($scope.order);
-          $location.path('/checkout');
-      };
+    $scope.continue = function () {
+      var id = Auth.getCurrentUser()._id;
+      shareData.store($scope.order);
+      console.log($scope.order)
+      $location.path('/checkout');
+    };
   }).filter('underscoreless', function () {
-  return function (input) {
+    return function (input) {
       return input.replace(/_/g, ' ');
-  };
-});
+    };
+  });
