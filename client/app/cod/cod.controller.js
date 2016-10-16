0b1227690160cb28fa3e3c84e3299a4747bad0ef
Khushmeet <khushmeetsingh1996@gmail.com>
2016-10-16 16:56:40 +0530

    fixed the User.orderUpdate

--- client/app/cod/cod.controller.js
+++ client/app/cod/cod.controller.js
@@ -1,14 +1,33 @@
 'use strict';
-(function(){
+(function () {
 
-function CodComponent($scope) {
-  $scope.message = 'Hello';
-}
+  function CodComponent($scope, shareData2, shareData, User, Auth, $mdToast) {
+    $scope.total = shareData2.get()
+    var order = shareData.get()
 
-angular.module('lakhaanaApp')
-  .component('cod', {
-    templateUrl: 'app/cod/cod.html',
-    controller: CodComponent
-  });
+    $scope.placeOrder = function (form) {
+      if (form) {
+        User.updateOrder({ id: Auth.getCurrentUser()._id }, { order: order },
+          function success(response) {
+            console.log(response)
+          },
+          function error(error) {
+            console.log(error)
+          })
+        var toast = $mdToast.simple()
+          .textContent('Your order is placed, Thank you!')
+          .action('OK')
+          .highlightAction(false)
+          .position('bottom');
+        $mdToast.show(toast);
+      }
+    }
+  }
+
+  angular.module('lakhaanaApp')
+    .component('cod', {
+      templateUrl: 'app/cod/cod.html',
+      controller: CodComponent
+    });
 
 })();
