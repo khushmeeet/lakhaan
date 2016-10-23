89660ae97a4ec05a74395b105ca38e90217a0806
Khushmeet <khushmeetsingh1996@gmail.com>
2016-10-24 00:56:52 +0530

    msg91 api query

--- client/app/cod/cod.controller.js
+++ client/app/cod/cod.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 (function () {
 
-  function CodComponent($scope, shareData2, shareData, User, Auth, $mdToast) {
+  function CodComponent($scope, shareData2, shareData, User, Auth, $mdToast, sendSMS) {
     $scope.total = shareData2.get()
     var order = shareData.get()
 
@@ -22,6 +22,16 @@
         $mdToast.show(toast);
       }
     }
+
+    sendSMS.query({
+      authkey: '129164AppiG3Vti4b580c95b8',
+      mobiles: '9944490100',
+      message: 'this is message',
+      sender: 'HP-FOODLE',
+      route: 4,
+      country: 91,
+      response: 'json'
+    });
   }
 
   angular.module('lakhaanaApp')
