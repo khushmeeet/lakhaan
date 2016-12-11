3c1cd69ed8931c607d2a087ae513a69af7a4ae24
Khushmeet <khushmeetsingh1996@gmail.com>
2016-12-11 12:12:39 +0530

    corrected sender ID

--- client/app/cod/cod.controller.js
+++ client/app/cod/cod.controller.js
@@ -4,7 +4,7 @@
   function CodComponent($scope, shareData2, shareData, User, Auth, $mdToast, sendSMS) {
     $scope.total = shareData2.get()
     var order = shareData.get()
-    console.log($scope.total,$scope.order)
+    console.log($scope.total,order)
     $scope.placeOrder = function (form) {
       if (form) {
         User.updateOrder({ id: Auth.getCurrentUser()._id }, { order: order },
@@ -22,9 +22,9 @@
         $mdToast.show(toast);
         sendSMS.query({
           authkey: '133386AMaB5ImNjl584ab985',
-          mobiles: '9944490100',
-          message: order.toString(),
-          sender: 'HP-FOODLE',
+          mobiles: '9161999900'+','+$scope.total[1],
+          message: JSON.stringify(order),
+          sender: 'FOODLE',
           route: 4,
           country: 91,
           response: 'json'
